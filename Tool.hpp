e034e8ff9311227414b2f3f7b6d10eaf5831a522
harrisri <harrisri@oregonstate.edu>
2017-02-18 13:48:22 -0800

    added comments

--- Tool.hpp
+++ Tool.hpp
@@ -1,8 +1,8 @@
 /**********************************************************************
-**Program Name:
-**Author:
-**Date:
-**Description:
+**Program Name:		RPS Game
+**Author:			Group 10
+**Date:				2/18/17
+**Description:		Rock class header file.
 **********************************************************************/
 
 #ifndef TOOL_HPP
@@ -11,24 +11,32 @@
 class Tool
 {
 public:
+	//default constructor
 	Tool();
+	//constructor
 	Tool(int);
+	//copy constructor
 	Tool(const Tool&);
+	//destructor
 	virtual ~Tool();
+	//assignment operator overload
 	void operator=(const Tool &RHS);
 
+	//setters
 	void setStrength(int);
 	void setType(char);
 
+	//getters
 	char getType();
 	char getType() const;
 	int getStrength();
 	int getStrength() const;
 
+	//function prototypes
 	char compareStrength(int, int);
 	virtual char fight(Tool&) = 0;
 
-protected:
+protected://protected member variables.
 	int strength;
 	char type;
 };
